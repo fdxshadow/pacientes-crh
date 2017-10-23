2eb7b61c797a22bf270bf6f6fd8215279e4bef7c
init20 <alfonsovargasguerra@gmail.com>
2017-10-22 23:38:14 -0300

    Update tipoexamen.client.controller.js

--- public/dashboard/controllers/tipoexamen.client.controller.js
+++ public/dashboard/controllers/tipoexamen.client.controller.js
@@ -1,10 +1,33 @@
-angular.module('dashboard').controller('tipoexamen',['$scope',function ($scope) {
-	$scope.examenes = [
-		{nombre: "Espermiograma", horario: "8 a 12 hrs y 14 a 17 hrs"},
-		{nombre: "Separación espermática",  horario: "8 a 12 hrs y 14 a 17 hrs"},
-		{nombre: "Fragmentación del DNA",  horario: "8 a 12 hrs y 14 a 17 hrs"},
-		{nombre: "Ecografía",  horario: "7 a 13 hrs"},
-		{nombre: "Inseminación intra uterina",  horario: "8 a 12 hrs y 14 a 17 hrs"},
-		{nombre: "Criopreservación espermática",  horario: "8 a 12 hrs y 14 a 17 hrs"}
-	]
+
+	examenResource.query(function(data){
+		$scope.examenes = data;
+		console.log(data);
+		if(data=="error getExamenes"){
+			$scope.examenes = {};
+		}
+	});
+	
+		servicemed.query(function(data){
+		$scope.medicos = data;
+
+
+	});
+
+
+
+		$scope.seleccionado = function(examen){
+		exameninfo.examen_id = examen._id;
+		exameninfo.examen_nombre = examen.nombre;
+		console.log(examen);
+
+		if(examen.nombre == "Ecografía"){
+			console.log(examen.nombre)
+			$location.url('/medico');
+		}
+		else{
+			pacienteinfo.medico_nombre = null;
+			$location.url('/reservaPaciente'); 
+		}
+}
+
 }]);

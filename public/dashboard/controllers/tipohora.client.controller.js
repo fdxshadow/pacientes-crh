735c30a1398179cd89f5d1029244b0a20bb4cf3a
fdxshadow <fran.peagonza@gmail.com>
2017-10-24 14:38:31 -0300

    mas detalles

--- public/dashboard/controllers/tipohora.client.controller.js
+++ public/dashboard/controllers/tipohora.client.controller.js
@@ -1,4 +1,11 @@
-angular.module('dashboard').controller('tipohora',['$scope',function ($scope) {
+angular.module('dashboard').controller('tipohora',['$scope','pacienteinfo',function ($scope,pacienteinfo) {
+
+
+	$scope.seleccionado= function(){
+		if(pacienteinfo.examen_id) pacienteinfo.examen_id=null;
+		console.log(pacienteinfo)
+
+	}
 }
 ]
 );

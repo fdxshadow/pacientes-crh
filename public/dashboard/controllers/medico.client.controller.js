75b7f7cc0f167ae32451add4f6ca1b5ed53fab9d
fdxshadow <fran.peagonza@gmail.com>
2017-10-24 14:12:26 -0300

    detalle

--- public/dashboard/controllers/medico.client.controller.js
+++ public/dashboard/controllers/medico.client.controller.js
@@ -14,6 +14,7 @@ angular.module('dashboard').controller('medico',['$scope','pacienteinfo','servic
     	console.log(medico);
         pacienteinfo.medico_id=medico._id;
         pacienteinfo.medico_nombre = medico.nombre;
+        if(pacienteinfo.examen_id) pacienteinfo.examen_id = null;
     }
  
 

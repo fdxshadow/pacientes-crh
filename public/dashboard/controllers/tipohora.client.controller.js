f2aeef70efbdea8b41273ab66eab62e31f8d972a
init20 <alfonsovargasguerra@gmail.com>
2017-10-30 00:10:49 -0300

    Update tipohora.client.controller.js

--- public/dashboard/controllers/tipohora.client.controller.js
+++ public/dashboard/controllers/tipohora.client.controller.js
@@ -1,6 +1,6 @@
 angular.module('dashboard').controller('tipohora',['$scope','pacienteinfo',function ($scope,pacienteinfo) {
 
-
+	$scope.paciente = pacienteinfo.paciente_nombre;
 	$scope.seleccionado= function(){
 		if(pacienteinfo.examen_id) pacienteinfo.examen_id=null;
 		console.log(pacienteinfo)

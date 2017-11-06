5c76ef7751749f81ec0345c3b785bbda620b1dfd
fdxshadow <fran.peagonza@gmail.com>
2017-11-06 19:08:26 -0300

    reserva por la linea de medico, falta modificar el back para que me traiga todas las reservas menos las rechazadas

--- public/reservaPaciente/controllers/reservaPaciente.client.controller.js
+++ public/reservaPaciente/controllers/reservaPaciente.client.controller.js
@@ -59,44 +59,87 @@ $scope.prueba = function(){
     if(validar.test($scope.date)){
         if(pacienteinfo.examen_id){
             var med = (pacienteinfo.medico_id)?pacienteinfo.medico_id:"no";
-            servicio.get({fecha:$scope.date,medico:med,examen:pacienteinfo.examen_id},function(response){
-                var disponibilidad1 = [];            
-                    var horas = parseInt(moment(pacienteinfo.examen_disponibilidad.hora_termino,"hh:mm").hours()) - parseInt(moment(pacienteinfo.examen_disponibilidad.hora_inicio,"HH:mm").hours())
-                    var resini =moment(pacienteinfo.examen_disponibilidad.hora_inicio,"h:mm");
-                    var ini = pacienteinfo.examen_disponibilidad.hora_inicio;
-                    var min = ini.split(":")[1];
-                    var hour= (moment(resini).add(1,'hours').hours()<10)?"0"+moment(resini).add(1,'hours').hours():moment(resini).add(1,'hours').hours()
-                    var fin = hour+":"+min;
-                    var aux = {
-                        hora_ini:ini,
-                        hora_term:fin
-                        };
-                    disponibilidad1.push(aux);
-                    for (var i = 1; i<horas; i++){
-                        var ino = disponibilidad1[disponibilidad1.length - 1].hora_term; 
-                        var resino = moment(ino,"hh:mm");
-                        var houro = (moment(resino).add(1,'hours').hours()<10)?"0"+moment(resino).add(1,'hours').hours():moment(resino).add(1,'hours').hours()
-                        var fino = houro+":"+min;
+            var disponibilidad1 = [];            
+            var horas = parseInt(moment(pacienteinfo.examen_disponibilidad.hora_termino,"hh:mm").hours()) - parseInt(moment(pacienteinfo.examen_disponibilidad.hora_inicio,"hh:mm").hours());                
+            var resini =moment(pacienteinfo.examen_disponibilidad.hora_inicio,"hh:mm");
+            var ini = pacienteinfo.examen_disponibilidad.hora_inicio;
+            var min = ini.split(":")[1];
+            var hour= (moment(resini).add(1,'hours').hours()<10)?"0"+moment(resini).add(1,'hours').hours():moment(resini).add(1,'hours').hours()
+            var fin = hour+":"+min;
+            var aux = {
+                hora_ini:ini,
+                hora_term:fin
+            };
+            disponibilidad1.push(aux);
+            for (var i = 1; i<horas; i++){
+                    var ino = disponibilidad1[disponibilidad1.length - 1].hora_term; 
+                    var resino = moment(ino,"hh:mm");
+                    var houro = (moment(resino).add(1,'hours').hours()<10)?"0"+moment(resino).add(1,'hours').hours():moment(resino).add(1,'hours').hours()
+                    var fino = houro+":"+min;
+                    var aux1 = {
+                        hora_ini:ino,
+                        hora_term:fino
 
-                        var aux1 = {
-                            hora_ini:ino,
-                            hora_term:fino
-
-                        }
-                        disponibilidad1.push(aux1);
                     }
-                console.log("esta son las reservas",response);
-                angular.forEach(response,function(reserva,key){
-                    angular.forEach(disponibilidad1,function(horario,key){
-                        if(reserva.hora_inicio_reserva==horario.hora_ini){
-                            disponibilidad1.splice(disponibilidad1.indexOf(horario),1);
-                        }
+                    disponibilidad1.push(aux1);
+            }
+            servicio.get({fecha:$scope.date,medico:med,examen:pacienteinfo.examen_id},function(response){
+              
+                    angular.forEach(response,function(reserva,key){
+                        angular.forEach(disponibilidad1,function(horario,key){
+                            if(reserva.hora_inicio_reserva==horario.hora_ini){
+                                disponibilidad1.splice(disponibilidad1.indexOf(horario),1);
+                            }
+                        });
                     });
-                });
                 $scope.horarios=disponibilidad1;
             
             });
-        }        
+        }
+        else{
+            if (pacienteinfo.medico_id) {
+                moment.locale("es");
+                var day = moment($scope.date,"YYYY-MM-DD");
+                var selectday=day.format("dddd").toLowerCase();
+                angular.forEach(pacienteinfo.medico_disponibilidad,function(disp,key){
+                    if (disp.dia == selectday) {
+                        var disponibilidad1=[];
+                        var horas = parseInt(moment(disp.hora_termino,"hh:mm").hours()) - parseInt(moment(disp.hora_inicio,"hh:mm").hours()); 
+                        var resini =moment(disp.hora_inicio,"hh:mm");
+                        var ini = disp.hora_inicio;
+                        var min = ini.split(":")[1];
+                        var hour= (moment(resini).add(1,'hours').hours()<10)?"0"+moment(resini).add(1,'hours').hours():moment(resini).add(1,'hours').hours()
+                        var fin = hour+":"+min;
+                        var aux = {
+                            hora_ini:ini,
+                            hora_term:fin
+                        };
+                        disponibilidad1.push(aux);
+                        for (var i = 1; i<horas; i++){
+                            var ino = disponibilidad1[disponibilidad1.length - 1].hora_term; 
+                            var resino = moment(ino,"hh:mm");
+                            var houro = (moment(resino).add(1,'hours').hours()<10)?"0"+moment(resino).add(1,'hours').hours():moment(resino).add(1,'hours').hours()
+                            var fino = houro+":"+min;
+                            var aux1 = {
+                                hora_ini:ino,
+                                hora_term:fino
+                            }
+                        disponibilidad1.push(aux1);
+                        }
+                        servicio.get({fecha:$scope.date,medico:pacienteinfo.medico_id,examen:pacienteinfo.examen_id},function(response){
+                            angular.forEach(response,function(reserva,key){
+                                angular.forEach(disponibilidad1,function(horario,key){
+                                    if(reserva.hora_inicio_reserva==horario.hora_ini){
+                                        disponibilidad1.splice(disponibilidad1.indexOf(horario),1);
+                                    }
+                                });
+                            });
+                        $scope.horarios=disponibilidad1;
+                        });
+                    }    
+                });
+            };
+        }
     }
 }    
 
